350f6474aeafcd65e09bbc8524e383f97995110e
Rofiu255 <mujeeb.rofiu@gmail.com>
2024-12-11 18:07:01 +0100

    updated

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-public class Program
-{
-    // Editing the same line (file from feature-FOF branch)
-    public static void Main(string[] args)
-    {
-        BuildWebHost(args).Run();
-    }
+var builder = WebApplication.CreateBuilder(args);
 
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
+// Add services to the  container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
